67b3b8989be10903a1e5f98ebe88eb8bfb1b2284
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2018-07-30 02:42:29 +0200

    Added custom scripts

---
+++ custom/dfnoise-build.ps1
@@ -0,0 +1,3 @@
+docker build -t grafana:latest-dfnoise \
+  --build-arg "GRAFANA_VERSION=latest" \
+  --build-arg "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource" .
---
+++ custom/dfnoise-run.ps1
@@ -0,0 +1,5 @@
+docker run \
+  -d \
+  -p 3000:3000 \
+  --name=dfnoisegrafana \
+  grafana:latest-dfnoise
